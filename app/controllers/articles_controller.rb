4db243e15cae94a1725990bb0772b093b6161847
Ehbraheem <ehbraheem@gmail.com>
2017-01-19 00:15:21 -0800

    create edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,19 @@ class ArticlesController < ApplicationController
     render 'new'
   end
 
+  def edit
+    @article = Article.find params[:id]
+  end
+
+  def update
+    @article = Article.find params[:id]
+    if @article.update article_params
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article) and return
+    end
+    render 'edit'
+  end
+
   def show
     @article = Article.find params[:id]
   end
