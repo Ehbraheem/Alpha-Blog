8676e8342f0bacb6123b63054e798066f5899820
Ehbraheem <ehbraheem@gmail.com>
2017-01-21 11:10:49 -0800

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: { minimum: 3, maximum: 25}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-_.]+@[a-z\d\-_.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 105},
+            format: { with: VALID_EMAIL_REGEX}
 
 
+  # TODO: Fix the validation to be dry
+  protected # to protect against external method call
+  def validation min, max
+
+  end
+  # End TODO: fix validation to be dry
 end
